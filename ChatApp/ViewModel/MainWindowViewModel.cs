6b36b0a250b2df2b825c51aed3e75f5699ce9805
IsaacKMHedman <hedmanisaac@gmail.com>
2025-11-12 18:35:40 +0100

    sd

--- ChatApp/ViewModel/MainWindowViewModel.cs
+++ ChatApp/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatApp.ViewModel
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
