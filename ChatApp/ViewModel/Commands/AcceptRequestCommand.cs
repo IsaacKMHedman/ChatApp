0179f89c0dfed0ac01230aef1902cf7a416d65b3
isahe131 <isahe131@student.liu.se>
2025-11-24 14:58:05 +0100

    asd

---
+++ ChatApp/ViewModel/Commands/AcceptRequestCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatApp.ViewModel.Commands
+{
+    internal class AcceptRequestCommand : ICommand
+    {
+
+        public event EventHandler CanExecuteChanged;
+        private AcceptRequestWindowViewModel parent = null;
+
+        public AcceptRequestCommand(AcceptRequestWindowViewModel parent)
+        {
+            this.parent = parent;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            parent.acceptRequestFunction();
+        }
+    }
+}
